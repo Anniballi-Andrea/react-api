16a82ed8a7f5f2bfd06d762a3efd544122810949
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-12 12:08:45 +0100

    add actress with ajax call in actress array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [actress, setActress] = useState([])
+
+  function fetchActress() {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((res) => setActress(res.data))
 
 
+  }
+  useEffect(fetchActress, [])
+
+  //console.log(actress)
 
   return (
     <>
