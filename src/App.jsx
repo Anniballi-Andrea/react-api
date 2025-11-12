0f2118f6da59d7b03d3d92bbc58d5c2f44dd9252
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-12 12:43:24 +0100

    add the info of the actress

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,13 @@ function App() {
                   <div className="card">
                     <img src={el.image} alt="" />
                   </div>
-
+                  <div className="card-body">
+                    <div><strong>Name:</strong> {el.name}</div>
+                    <div><strong>Birth year:</strong> {el.birth_year}</div>
+                    <div><strong>Nationality:</strong> {el.nationality}</div>
+                    <div><strong>Biography:</strong> {el.biography}</div>
+                    <div><strong>Awards:</strong> {el.awards}</div>
+                  </div>
                 </div>
               ))
             }
